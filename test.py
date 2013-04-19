688b9f2e434b4a4010002facd1a72fc294422608
Craig McCulloch <cm@pyridoxus.com>
2013-04-19 11:22:28 -0500

    Simple Agilent 33220A object works.

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ class Keithley2750(vxi_11_connection):
     Define a simple interface to a Keithley 2750.
     '''
     def __init__(self, host = '127.0.0.1', device = "inst0", timeout = 1000,
-                raise_on_err = None, device_name="Network Device",
+                raise_on_err = None, device_name="Keithley 2750",
                 shortname = None, portmap_proxy_host = None,
                 portmap_proxy_port = PMAP_PORT):
         '''
@@ -35,10 +35,42 @@ class Keithley2750(vxi_11_connection):
         print self.idn
         
 
+class Agilent33220A(vxi_11_connection):
+    '''
+    Define a simple interface to an Agilent Technologies,33220A.
+    '''
+    def __init__(self, host = '127.0.0.1', device = "inst0", timeout = 1000,
+                raise_on_err = None, device_name="Agilent Technologies,33220A",
+                shortname = None, portmap_proxy_host = None,
+                portmap_proxy_port = PMAP_PORT):
+        '''
+        Initialize the object
+        '''
+        # idn_head is used to check that the correct instrument is at the
+        # correct GPIB address. This check is done in the constructor of
+        # vxi_11_connection.
+        self.idn_head = "Agilent Technologies,33220A"
+         
+        vxi_11_connection.__init__(self, host, device, timeout, raise_on_err,
+                                   device_name, shortname, portmap_proxy_host,
+                                   portmap_proxy_port)
+        
+        
+    def getIDN(self):
+        '''
+        Print the IDN information.
+        '''
+        print self.idn
+        
+
 if __name__ == "__main__":
     dmm = Keithley2750(host="172.24.24.30", 
-             device="gpib0,16",  timeout=5000, device_name="Keithley 2750",
-            raise_on_err=1)
+             device="gpib0,16",  timeout=5000, raise_on_err=1)
     
     dmm.getIDN()
+
+    fgen = Agilent33220A(host="172.24.24.30", 
+             device="gpib0,10",  timeout=5000, raise_on_err=1)
+    
+    fgen.getIDN()
     
