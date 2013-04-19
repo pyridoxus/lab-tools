35a60a1ec52dae045230941951773af116b90d33
Craig McCulloch <cm@pyridoxus.com>
2013-04-19 10:55:21 -0500

    Simple Keithley 2750 object works.

--- test.py
+++ test.py
@@ -5,23 +5,34 @@ Created on Apr 12, 2013
 '''
 
 from data_acquisition.vxi_11 import vxi_11_connection
+from data_acquisition.rpc import PMAP_PORT
 
 class Keithley2750(vxi_11_connection):
     '''
     Define a simple interface to a Keithley 2750.
     '''
-    def __init__(self):
+    def __init__(self, host = '127.0.0.1', device = "inst0", timeout = 1000,
+                raise_on_err = None, device_name="Network Device",
+                shortname = None, portmap_proxy_host = None,
+                portmap_proxy_port = PMAP_PORT):
         '''
         Initialize the object
         '''
-        self.__idn = ""
+        # idn_head is used to check that the correct instrument is at the
+        # correct GPIB address. This check is done in the constructor of
+        # vxi_11_connection.
+        self.idn_head = "KEITHLEY INSTRUMENTS"
+         
+        vxi_11_connection.__init__(self, host, device, timeout, raise_on_err,
+                                   device_name, shortname, portmap_proxy_host,
+                                   portmap_proxy_port)
         
         
     def getIDN(self):
         '''
         Print the IDN information.
         '''
-        print self.__idn
+        print self.idn
         
 
 if __name__ == "__main__":
