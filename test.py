4a67b53ca2e1c2f40b4f78f1153499e56f0b8379
Craig McCulloch <cm@pyridoxus.com>
2013-04-19 11:39:32 -0500

    Simple Texio load object works.

--- test.py
+++ test.py
@@ -91,19 +91,52 @@ class KrohnHite523(vxi_11_connection):
         print self.idn
         
 
+class TEXIO_PEL151_201(vxi_11_connection):
+    '''
+    Define a simple interface to a TEXIO,PEL151-201.
+    '''
+    def __init__(self, host = '127.0.0.1', device = "inst0", timeout = 1000,
+                raise_on_err = None, device_name="Krohn-Hite, Model 523",
+                shortname = None, portmap_proxy_host = None,
+                portmap_proxy_port = PMAP_PORT):
+        '''
+        Initialize the object
+        '''
+        # idn_head is used to check that the correct instrument is at the
+        # correct GPIB address. This check is done in the constructor of
+        # vxi_11_connection.
+        self.idn_head = "*IDN TEXIO,PEL151-201"
+         
+        vxi_11_connection.__init__(self, host, device, timeout, raise_on_err,
+                                   device_name, shortname, portmap_proxy_host,
+                                   portmap_proxy_port)
+        
+        
+    def getIDN(self):
+        '''
+        Print the IDN information.
+        '''
+        print self.idn
+        
+
 if __name__ == "__main__":
     dmm = Keithley2750(host="172.24.24.30", 
-             device="gpib0,16",  timeout=5000, raise_on_err=1)
+             device="gpib0,16", raise_on_err=1)
     
     dmm.getIDN()
-
+    
     fgen = Agilent33220A(host="172.24.24.30", 
-             device="gpib0,10",  timeout=5000, raise_on_err=1)
+             device="gpib0,10", raise_on_err=1)
     
     fgen.getIDN()
  
     dcsource = KrohnHite523(host="172.24.24.30", 
-             device="gpib0,2",  timeout=5000, raise_on_err=1)
+             device="gpib0,2", raise_on_err=1)
     
     dcsource.getIDN()
-   
+ 
+    eload = TEXIO_PEL151_201(host="172.24.24.30", 
+             device="gpib0,13", raise_on_err=1)
+    
+    eload.getIDN()
+    
