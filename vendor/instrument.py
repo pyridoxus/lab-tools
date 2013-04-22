5bf3aef14605078a46fa68eb512a65698eb2292f
Craig McCulloch <cm@pyridoxus.com>
2013-04-22 15:15:45 -0500

    Working on getting MAC address from Ethernet instruments. Shifting over to Linux.

--- vendor/instrument.py
+++ vendor/instrument.py
@@ -4,7 +4,7 @@ Base abstract instrument classes to join all Vendor and SoMat instrument objects
 This must become a piece of CTHULU.
 '''
 
-def get_macaddress(host):
+def getMACAddress(host):
     """ Returns the MAC address of a network host, requires >= WIN2K. """
     
     import ctypes
@@ -46,15 +46,21 @@ def get_macaddress(host):
     return macaddr.upper()
 
 class Instrument(object):
-    
+    '''
+    Base instrument class.
+    '''
     def _getUniqueIdent(self):
+        ''' To be implemented by child classes. '''
         raise NotImplementedError, self
 
 
 
 class gpibInstrument(Instrument):
-    
+    '''
+    Subclass of instrument for GPIB type classes.
+    '''
     def _getUniqueIdent(self, instrObj):
+        ''' Return the result of asking the instrument for its ID. '''
         gpibIdent = instrObj.target.ask("*IDN?")
         
         manufSN = gpibIdent.split(",")[2]
@@ -64,10 +70,18 @@ class gpibInstrument(Instrument):
 
 
 class ethernetInstrument(Instrument):
-    
+    '''
+    Subclass of instrument for Ethernet type classes.
+    '''
     def _getUniqueIdent(self, instrObj):
-        macIdent = get_macaddress(instrObj.ip_address)
+        '''
+        Return the MAC address of the instrument.
+        '''
+        macIdent = getMACAddress(instrObj.ip_address)
         
         return macIdent
 
-        
+
+
+if __name__ == "__main__":
+    print getMACAddress("172.24.24.40")
