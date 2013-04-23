1b4142121895e5a7e5c39f3cd40db9e212e9e76b
Craig McCulloch <cm@pyridoxus.com>
2013-04-23 11:36:36 -0500

    Able to get MAC from /proc/net/arp.

--- vendor/instrument.py
+++ vendor/instrument.py
@@ -3,47 +3,34 @@ Base abstract instrument classes to join all Vendor and SoMat instrument objects
 
 This must become a piece of CTHULU.
 '''
+import socket
+     
+def getHwAddr(interface, host, port = 80):
+    ''' Get the MAC address of the device at the host IP address. '''
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #Connect to remote server to refresh the ARP file
+    s.connect((host , port))
+    
+    arpFile = open("/proc/net/arp", 'r')
+    
+    mac = None
+    itemList = None
+    for line in arpFile:
+        if interface in line:
+            if host in line:
+                itemList = line.split(" ")
+                break
+    arpFile.close()
 
-def getMACAddress(host):
-    """ Returns the MAC address of a network host, requires >= WIN2K. """
+    if itemList is not None:
+        for item in itemList:
+            # Go through all pieces of the line and find the MAC address
+            # It's the only item with a colon.
+            if ":" in item:
+                mac = item
+    return mac
     
-    import ctypes
-    import socket
-    import struct
-    # Check for api availability
-    try:
-        SendARP = ctypes.windll.Iphlpapi.SendARP
-    except:
-        raise NotImplementedError('Usage only on Windows 2000 and above')
-        
-    # Doesn't work with loopbacks, but let's try and help.
-    if host == '127.0.0.1' or host.lower() == 'localhost':
-        host = socket.gethostname()
-    
-    # gethostbyname blocks, so use it wisely.
-    try:
-        inetaddr = ctypes.windll.wsock32.inet_addr(host)
-        if inetaddr in (0, -1):
-            raise Exception
-    except:
-        hostip = socket.gethostbyname(host)
-        inetaddr = ctypes.windll.wsock32.inet_addr(hostip)
-    
-    buff = ctypes.c_buffer(6)
-    addlen = ctypes.c_ulong(ctypes.sizeof(buff))
-    if SendARP(inetaddr, 0, ctypes.byref(buff), ctypes.byref(addlen)) != 0:
-        raise 'Retrieval of mac address(%s) - failed' % host
-    
-    # Convert binary data into a string.
-    macaddr = ''
-    for intval in struct.unpack('BBBBBB', buff):
-        if intval > 15:
-            replacestr = '0x'
-        else:
-            replacestr = 'x'
-        macaddr = ''.join([macaddr, hex(intval).replace(replacestr, '')])
-    
-    return macaddr.upper()
+
 
 class Instrument(object):
     '''
@@ -77,11 +64,12 @@ class ethernetInstrument(Instrument):
         '''
         Return the MAC address of the instrument.
         '''
-        macIdent = getMACAddress(instrObj.ip_address)
+        macIdent = getHwAddr(instrObj.ip_address)
         
         return macIdent
 
 
 
 if __name__ == "__main__":
-    print getMACAddress("172.24.24.40")
+    print getHwAddr("eth0", "172.24.24.40")
+    print getHwAddr("eth0", "172.24.24.25")    
