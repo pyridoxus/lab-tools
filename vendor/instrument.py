bc8259b62f935f610cde62405bc157310a6d6ea5
Craig McCulloch <cm@pyridoxus.com>
2013-04-23 11:44:55 -0500

    Able to handle exception if nothing at an IP.

--- vendor/instrument.py
+++ vendor/instrument.py
@@ -6,14 +6,22 @@ This must become a piece of CTHULU.
 import socket
      
 def getHwAddr(interface, host, port = 80):
-    ''' Get the MAC address of the device at the host IP address. '''
+    '''
+    Get the MAC address of the device at the host IP address.
+    Return None if no connection can be made (either bad IP or bad port)
+    Return "INVALID" if no MAC address can be found.
+    Return a valid MAC address if it is found.
+    '''
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     #Connect to remote server to refresh the ARP file
-    s.connect((host , port))
+    try:
+        s.connect((host , port))
+    except socket.error:
+        return None
     
     arpFile = open("/proc/net/arp", 'r')
     
-    mac = None
+    mac = "INVALID"
     itemList = None
     for line in arpFile:
         if interface in line:
@@ -72,4 +80,6 @@ class ethernetInstrument(Instrument):
 
 if __name__ == "__main__":
     print getHwAddr("eth0", "172.24.24.40")
+    print getHwAddr("eth0", "172.24.24.30")
+    print getHwAddr("eth0", "172.24.24.20")
     print getHwAddr("eth0", "172.24.24.25")    
