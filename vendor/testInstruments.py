8d4c09c8df9c4fc331288bce34082e7f406451c4
Craig McCulloch <cm@pyridoxus.com>
2013-07-24 11:03:31 -0500

    Changed GPIB address of desired instrument.

--- vendor/testInstruments.py
+++ vendor/testInstruments.py
@@ -7,7 +7,7 @@ Created on Jul 3, 2013
 from powerSupplies import PowerSupplyInstekPsm6003
 from time import sleep
 if __name__ == "__main__":
-    powerSupply = PowerSupplyInstekPsm6003("172.24.24.30", "gpib0,16", 1)
+    powerSupply = PowerSupplyInstekPsm6003("172.24.24.30", "gpib0,9", 1)
     powerSupply.setVoltage(12, 2, 2)
     sleep(10)
     powerSupply.inputOff()
